5880320923fa10440bf0933b720713d114844aaa
Slav <stan458@abv.bg>
2022-06-26 14:47:38 +0300

    working on error handlers

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
 const path = require('path');
 // third party packages
 const express = require('express');
+const csrf = require('csurf');
 
 // custom middlewares
-const authRoutes = require('./routes/auth.routes');
-
+const db = require("./data/database");
+const addCsrfTokenMiddleware = require('./middlewares/csrf-token');
+const errorHandlerMiddleware = require('./middlewares/error-handler');
+const authRoutes = require("./routes/auth.routes");
 
 const app = express();
 
-// ejs view engine setup 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+// ejs view engine setup
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 // built in static midllerware
-app.use(express.static('public'));
+app.use(express.static("public"));
+app.use(express.urlencoded({exteneded: false}));
+
+// third/party middleware csurf helps generate the token + checks incoming tokens for validity
+app.use(csrf());
+// our mwre just distributes generated tokens to all our other middleware/route handler functions and viwes
+app.use(addCsrfTokenMiddleware);  
 
 app.use(authRoutes);
 
-app.listen(3000);
+// server error handler
+app.use(errorHandlerMiddleware);
+
+db.connectToDatabase()
+  .then(function () {
+    app.listen(3000);
+  })
+  .catch(function (error) {
+    console.log("Failed to connect to the database!");
+    console.log(error);
+  });
--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,12 +1,30 @@
+const User = require("../models/user.model");
+
 function getSignup(req, res, next) {
-    res.render('client/auth/signup');
-} 
+  res.render("client/auth/signup");
+}
+
+async function signup(req, res) {
+  const user = new User(
+    req.body.email,
+    req.body.password,
+    req.body.fullname,
+    req.body.street,
+    req.body.postal,
+    req.body.city
+  );
+
+  await user.signup()
+
+  res.redirect('/login');
+}
 
 function getLogin(req, res, next) {
-    // ...
-}   
+  res.render("client/auth/login");
+}
 
 module.exports = {
-    getSignup: getSignup,
-    getLogin: getLogin
-}
+  getSignup: getSignup,
+  getLogin: getLogin,
+  signup: signup,
+};
